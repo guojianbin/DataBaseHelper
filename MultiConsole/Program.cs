6ada4846eed46d18a1606a1bb0497dcea9dfaa8c
晨星宇 <1025395601@qq.com>
2015-09-23 16:13:33 +0800

    更新的Git保存，

--- MultiConsole/Program.cs
+++ MultiConsole/Program.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
-using DbHelper.HelperSome;
 using System;
 using MySql.Data.MySqlClient;
 using DbHelper;
